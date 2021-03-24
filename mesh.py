6c81f7405d03cba78d3153462562608cbf7ef109
Yiftach Edelstain <yedelstain@apple.com>
2021-03-24 15:23:48 +0200

    add vert degree and v_v adj

--- mesh.py
+++ mesh.py
@@ -1,5 +1,7 @@
 from off_parser import read_off,write_off
-from scipy.sparse import lil_matrix 
+from scipy.sparse import lil_matrix ,identity
+import numpy as np
+import pyvista as pv
 class Mesh(object):
     def __init__(self,path):
         vertices_faces =  read_off(path)
@@ -10,5 +12,14 @@ class Mesh(object):
         for face_index,face in enumerate(self.f):
             for vert_index in face:
                 adjacency[vert_index,face_index]=True
-        return adjacency
-print(Mesh("example.off").vertex_face_adjacency().todense())
+        return adjacency > 0
+    def vertex_vertex_adjacency(self):
+        v_f_adjacency = self.vertex_face_adjacency()
+        return (v_f_adjacency*v_f_adjacency.transpose() - identity(len(self.v)))>0
+    def vertex_degree(self):
+        v_v_adjecency = self.vertex_vertex_adjacency()
+        return v_v_adjecency*np.ones(len(self.v))
+    def render_wireframe(self):
+        data = pv.DataSet(self)
+
+print(Mesh("example.off").render_wireframe())
