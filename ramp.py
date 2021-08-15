3087e95aceb29c96563c3663eb74c0107fd60481
unknown <hadasro811@gmail.com>
2021-08-15 10:28:51 +0300

    added gpu function

--- ramp.py
+++ ramp.py
@@ -5,6 +5,7 @@ import os
 import shutil
 import torch
 import cpuinfo
+from tensorflow.python.client import device_lib
 
 def identify_system():
     print("Computer Name :" , socket.gethostname())
@@ -15,10 +16,16 @@ def identify_system():
     print("Free Disk Space : ", "{:.1f}".format(shutil.disk_usage("C:").free // (2**30)), "GB")
     print("Compute Packages : ", "PyTorch", torch.__version__[0:5], )
     print("# Visible GPUs :" )
-    phy_cores = psutil.cpu_count(logical=False)
-    log_cores = psutil.cpu_count(logical=True)
-    vram = psutil.virtual_memory()[0]
-    free_space = "{:.1f}".format(shutil.disk_usage("C:").free // (2**30))
+    #phy_cores = psutil.cpu_count(logical=False)
+    #log_cores = psutil.cpu_count(logical=True)
+    #vram = psutil.virtual_memory()[0]
+    #free_space = "{:.1f}".format(shutil.disk_usage("C:").free // (2**30))
+
+def get_available_gpus():
+    local_device_protos = device_lib.list_local_devices()
+    return [x.name for x in local_device_protos if x.device_type == 'GPU']
+
+print(get_available_gpus())
 
 identify_system()
 
