6fc31a9dcecb57a32cbd2b0a3e5620a2ca214366
ido1 imanuel <ido.iGIP1@csf.technion.ac.il>
2021-09-08 16:54:02 +0300

    starting commit here

---
+++ LightNet/light/train.py
@@ -0,0 +1,32 @@
+import LightNet.light.model as model
+from LightNet.light.dataset import ShapeNetDataset
+import torch
+
+import pytorch_lightning as pl
+        # d = ShapeNetDataset(root = datapath, classification = True)
+
+dataset = ShapeNetDataset(
+    root="D:\\projectHY\\Ramp\\LightNet\\pointnet.pytorch\\shapenetcore_partanno_segmentation_benchmark_v0",
+    classification=True)
+dataloader = torch.utils.data.DataLoader(
+    dataset,
+    batch_size=32,
+    shuffle=True,
+    num_workers=int(0))
+
+test_dataset = ShapeNetDataset(
+    root="D:\\projectHY\\Ramp\\LightNet\\pointnet.pytorch\\shapenetcore_partanno_segmentation_benchmark_v0",
+    classification=True,
+    split='test',
+    data_augmentation=False)
+testdataloader = torch.utils.data.DataLoader(
+    test_dataset,
+    batch_size=32,
+    shuffle=True,
+    num_workers=int(0))
+
+trainer = pl.Trainer(gpus=1,max_epochs=10)
+model = model.LightNetCls(k=16,feature_transform=False)
+
+trainer.fit(model, dataloader)
+trainer.test(model,testdataloader)
